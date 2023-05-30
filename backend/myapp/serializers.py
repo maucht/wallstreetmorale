df463680ba5b600c04e275b365bdbc4ed320475a
Tyler <tylermauch18@gmail.com>
2023-05-30 13:09:57 -0400

    moved backend and scripts out of repo

--- backend/myapp/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import DailyStats
-
-class DataSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = DailyStats
-        fields = '__all__'
