086ce1487707b6e28b21f5a5d1cc44bf8cffc8e2
Tyler <tylermauch18@gmail.com>
2023-06-07 22:17:19 -0400

    audit completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -33,22 +33,47 @@ export default class Home extends Component {
   }
 
   setRate = () => {
+    // EXPERIMENTAL START
+    const { exec } = require('child_process');
+    exec('cd ../../pyscript && python main.py', (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.error(`stderr: ${stderr}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+    });
+
+    // EXPERIMENTAL END
+
     let instance = axios.create({
       baseURL: 'https://wsmapi.onrender.com/',
     });
 
     instance.get('api/dailystats').then(response => {
       console.log(response.data.datas[0])
-      const posStat = response.data.datas[0]['posStat']
-      const negStat = response.data.datas[0]['negStat']
+      const resLength = response.data.datas.length
+      const posStat = response.data.datas[resLength - 1]['posStat']
+      const negStat = response.data.datas[resLength - 1]['negStat']
+
+      const prevPosStat = response.data.datas[resLength - 2]['posStat']
+      const prevNegStat = response.data.datas[resLength - 2]['negStat']
 
       this.setState({
         posStat,
         negStat,
+        prevPosStat,
+        prevNegStat
       }, () => {
         const rate = -1 + 2 * (this.state.posStat / (this.state.posStat + this.state.negStat));
+        const prevRate = -1 + 2 * (this.state.prevPosStat / (this.state.prevPosStat + this.state.prevNegStat));
+        console.log("PREV RATE:",prevRate)
         this.setState({
           rate: rate,
+          prevRate,
           statsLoaded: true,
         }, ()=>{
           this.movePointer()
@@ -83,7 +108,7 @@ export default class Home extends Component {
         }
         else{
           pointer.style.marginLeft = "0px"
-          pointer.style.marginRight = `${calculatedMovement}px`
+          pointer.style.marginRight = `${-calculatedMovement}px`
         }
         
       }
@@ -126,8 +151,8 @@ export default class Home extends Component {
         <div className="flex items-center justify-center text-center relative h-full flex-col">
           <h1 className="text-white text-5xl font-mono font-bold pb-12">r/Wallstreetbets mood of the day:</h1>
 
-          <div className="rounded-lg min-h-32 max-h-32 flex flex-col align-bottom pl-12 pr-12" ref={this.meter}>
-            <img src="meterv2.png" alt="Mood Meter"  className="h-full" />
+          <div className="rounded-lg min-h-32 max-h-32 flex flex-col align-bottom" ref={this.meter}>
+            <img src="meterv2.png" alt="Mood Meter"  className="h-full w-full" />
           </div>
 
           <div className="h-48" ref={this.pointer}>
@@ -136,19 +161,25 @@ export default class Home extends Component {
 
           <div id = "infoBubble" className = " h-42 w-96 bg-white rounded-3xl border-8 flex items-center p-8 flex-row">
             <h1 id = "rateNumDisplay" 
-            className = {(this.state.rate > 0 ? "text-green-600" : "text-red-700") + " font-nunito-bold text-5xl order-1"}
+            className = {(this.state.rate > 0 ? "text-green-600" : "text-red-700") + " font-nunito-bold text-4xl order-1"}
             
-            >{(this.state.posStat / (this.state.posStat + this.state.negStat)).toPrecision(2).toString().substring(2)}%</h1>
+            >{(this.state.rate !== 1.00) ? 
+              this.state.posStat / (this.state.posStat + this.state.negStat).toPrecision(2).toString().substring(2) :
+              this.state.posStat / (this.state.posStat + this.state.negStat).toPrecision(2).toString().substring(0) + "00"
+            }%</h1>
 
             <div className=" h-3/4 border-r border-gray-400 border-2 relative left-1/2 order-2"></div>
             <h1 id = "rateCategory"
-            className = {(this.state.rate > 0 ? "text-green-600" : "text-red-700") + " relative font-nunito-bold text-3xl order-3 left-8"}
+            className = {(this.state.rate > 0 ? "text-green-600" : "text-red-700") + " relative font-nunito-bold text-2xl order-3 left-6"}
             >{this.categorizeRate()}</h1>
 
             <h1
             id = "rateDelta"
             className = " order-4 font-nunito-bold text-2xl relative left-1/3"
-            >+7%</h1>
+            >{(this.state.prevRate !== 1.00) ? 
+              this.state.prevPosStat / (this.state.prevPosStat + this.state.prevNegStat).toPrecision(2).toString().substring(2) :
+              this.state.prevPosStat / (this.state.prevPosStat + this.state.prevNegStat).toPrecision(2).toString().substring(0) + "00"
+            }%</h1>
 
           </div>
 
